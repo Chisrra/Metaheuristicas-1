290043f1a373bfb091e01f19a640e54cd54e5576
Chisrra <75094536+Chisrra@users.noreply.github.com>
2023-08-28 23:48:30 -0600

    Develop the One Max class and gitignore

--- max_one.py
+++ max_one.py
@@ -1,10 +1,11 @@
 import random
 
 # Parametros
-NUM_CROMOSOMAS = 100
+NUM_CROMOSOMAS = 50
 NUM_GENES = 20
 NUM_GENERACIONES = 200
-PROB_MUTACION = 0.5
+PROB_CRUCE = 0.8
+PROB_MUTACION = 0.05
 PORCENTAJE_ELITISMO = 0.1
 
 # Métodos
@@ -25,8 +26,8 @@ def cruzar(padre1:list, padre2:list):
 
 def mutar(cromosoma:list):
     for i in range(NUM_GENES):
-        if(random.random() > PROB_MUTACION):
-            cromosoma[i] = 1 - cromosoma[i] # Cambiamos el valor de 1 a 0 y viceversa
+        if(random.random() <= PROB_MUTACION):
+            cromosoma[i] = 1 - cromosoma[i] #Cambiamos el valor de 1 a 0 y viceversa
 
 # Implementación
 poblacion = [crear_cromosoma() for _ in range(NUM_CROMOSOMAS)]
@@ -38,18 +39,18 @@ for generacion in range(NUM_GENERACIONES):
     nueva_poblacion.extend(sorted(poblacion, key=evaluar_fitness, reverse=True)[:num_elitismo])
 
     for _ in range(NUM_CROMOSOMAS // 2):
-        padre1 = seleccion(poblacion)
-        padre2 = seleccion(poblacion)
+        if random.random() <= PROB_CRUCE: 
+            padre1 = seleccion(poblacion)
+            padre2 = seleccion(poblacion)
+            
+            hijo1, hijo2 = cruzar(padre1, padre2)
 
-        hijo1, hijo2 = cruzar(padre1, padre2)
+            mutar(hijo1)
+            mutar(hijo2)
 
-        mutar(hijo1)
-        mutar(hijo2)
-
-        nueva_poblacion.extend([hijo1, hijo2])
+            nueva_poblacion.extend([hijo1, hijo2])
 
     poblacion = nueva_poblacion
-    print(len(poblacion))
 
 mejor_cromosoma = max(poblacion, key=evaluar_fitness)
 print("Cromosoma óptimo: ", mejor_cromosoma)
